080921efda130efec98b11c3509c3d8c3577dcec
JesusAbarcaRodriguez <jesus.abarca.rodriguez@est.una.ac.cr>
2023-08-30 16:08:41 -0600

    [close_window & execute_code]

--- controller/ui_controller.py
+++ controller/ui_controller.py
@@ -5,3 +5,14 @@ class MainView(QMainWindow):
     def __init__(self):  # this
         super(MainView, self).__init__()  # Inicializa la clase -> Initialize
         uic.loadUi("view/view.ui", self)
+
+    # Conectar la señal clicked del botón "Exit" a la función que cierra la ventana
+        self.btn_exit.clicked.connect(self.close_window)
+        self.btn_execute.clicked.connect(self.execute_code)
+
+    def close_window(self):
+        self.close()
+
+    def execute_code(self):
+        code = self.textEdit.toPlainText()
+        self.textEdit_2.setPlainText(code)
