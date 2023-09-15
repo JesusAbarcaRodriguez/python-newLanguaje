3533cc5251cb05e40ce2bf1eef0dbbc936359dc8
JesusAbarcaRodriguez <jesus.abarca.rodriguez@est.una.ac.cr>
2023-09-14 23:06:43 -0600

    [UPDATE semantic_analysis]
    Ya se hacen asignaciones a variables, falta cuando es mas de una asignación, o sea: cuando es del tipo a= b+c*d.
    Se dejaron los elif para los for, while and if. Hay que pensare bien eso

--- functions/semantic_analysis/semantic_analysis.py
+++ functions/semantic_analysis/semantic_analysis.py
@@ -23,26 +23,61 @@ def semantic_analysis(tokens):
                 if is_assignment(tokens,i):
                     if is_declared_variable(tokens,i,principal):
                         variable_to_assign = principal.variables[tokens[i][1]]
-                        variable_to_assigni += 2
+                        index_variable_to_assign = i
+                        total_to_asiign = []
+                        i += 2
                         while tokens[i][0] != 'FIN_DE_INSTRUCCION':
                             if tokens[i][0] == 'IDENTIFICADOR':
                                 if tokens[i][1] in principal.variables and not principal.variables[tokens[i][1]][1] == None  and is_same_type(variable_to_assign,tokens,i,principal):
-                                    tokenAuxType = principal.variables[tokens[i][1]][0]
+                                    #hacer algoritmo para asignar a variable si son varias sumas,restas, mul,divi
+                                    total_to_asiign = principal.variables[tokens[i][1]][1]
                                     i += 1
                                 else:
                                     return f"Error semantico en {tokens[i][1]}"
                             elif tokens[i][0] == 'NUMERO_ENTERO':
-                                variable_to_assign[1]
                                 if variable_to_assign[0] == 'ENTERO':
-                                    principal.variables[tokens[i-2][1]][1] = tokens[i][1]
+                                    #hacer algoritmo para asignar a variable si son varias sumas,restas, mul,divi
+                                    total_to_asiign.append(int(tokens[i][1]))
                                     i += 1
+                            elif tokens[i][0] == "OPERADOR_ARITMETICO": i += 1 #agregar operadores a una pila
+                            elif tokens[i][0] == "CARACTER":
+                                if variable_to_assign[0] == 'CARACTER':
+                                    principal.variables[tokens[index_variable_to_assign][1]][1] = tokens[i][1]
+                            elif tokens[i][0] == "CADENA_LITERAL":
+                                if variable_to_assign[0] == 'CADENA':
+                                    principal.variables[tokens[index_variable_to_assign][1]][1] = tokens[i][1]
+                        #principal.variables[tokens[index_variable_to_assign][1]][1] = total_to_asiign
                     else:
                         return f"Error semantico en {tokens[i][1]}"
+                elif is_for(tokens,i):
+                    #agregar todo lo del for
+                    i += 1
+                elif is_while(tokens,i):
+                    #agregar todo lo del for
+                    i += 1
+                elif is_if(tokens,i):
+                    #agregar todo lo del for
+                    i += 1
                 if tokens[i][0] == 'INICIO':
                     countINICIO += 1
                 if tokens[i][0] == 'FIN':
                     countFIN += 1
                 i += 1
+def is_while(tokens,i):
+    if tokens[i][0] == 'MIENTRAS':
+        return True
+    else:
+        return False
+def is_if(tokens,i):
+    if tokens[i][0] == 'CUANDO':
+        return True
+    else:
+        return False
+def is_for(tokens,i):
+    if tokens[i][0] == 'PARA':
+        return True
+    else:
+        return False
 def is_variable_declaration(tokens,i):
     if tokens[i][0] == 'TIPO_DATO' and tokens[i+1][0] == 'IDENTIFICADOR':
         return True
