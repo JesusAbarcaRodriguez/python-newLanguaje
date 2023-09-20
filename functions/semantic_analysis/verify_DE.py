b6fef98a5f93f2248c22b9d8a6e41bfbead043a6
JesusAbarcaRodriguez <jesus.abarca.rodriguez@est.una.ac.cr>
2023-09-20 13:39:26 -0600

    Update verify_DE.py
    Se cambio para que solo se verifiquen tipos, en el folder executable_code se verificara que los valores estén bien y recorrer el rango

--- functions/semantic_analysis/verify_DE.py
+++ functions/semantic_analysis/verify_DE.py
@@ -1,24 +1,29 @@
 def verify_DE(principal,i,tokens):
     from functions.semantic_analysis.traverse_structure.traverse_structure import traverse_structure
     from functions.utils.utils import  is_declared_variable
-    init = 0
-    end = 0
-    if tokens[i][0] == 'NUMERO_ENTERO':
-        init = int(tokens[i][1])
-    elif tokens[i][0] == "IDENTIFICADOR":
+    variable_init = 0
+    variable_fin = 0
+    if tokens[i][0] == 'IDENTIFICADOR':
         if is_declared_variable(tokens,i,principal.variables):
-            init = principal.variables[tokens[i][1]][1]
+            variable_init = principal.variables[tokens[i][1]][0]
+    elif tokens[i][0] == 'NUMERO_ENTERO':
+        variable_init = int(tokens[i][1])
+    elif tokens[i][0] == 'NUMERO_FLOTANTE':
+        variable_init = float(tokens[i][1])
+    else:
+        return f"Error semantico en {tokens[i][1]} No se puede recorrer un rango con un tipo de dato {tokens[i][0]}"
     i += 2
-    if tokens[i][0] == 'NUMERO_ENTERO':
-        end = int(tokens[i][1])
-    elif tokens[i][0] == "IDENTIFICADOR":
-        if is_declared_variable(tokens,i,principal.variables):
-            end = principal.variables[tokens[i][1]][1]
-    i += 2
-    auxi = i
-    while init < end:
-        i = auxi
-        init += 1
+    if tokens[i][0] == 'IDENTIFICADOR':
+        if is_declared_variable(tokens,i+2,principal.variables):
+            variable_fin = principal.variables[tokens[i+2][1]][0]
+    elif tokens[i][0] == 'NUMERO_ENTERO':
+        variable_fin = int(tokens[i][1])
+    elif tokens[i][0] == 'NUMERO_FLOTANTE':
+        variable_init = float(tokens[i][1])
+    else:
+        return f"Error semantico en {tokens[i][1]} No se puede recorrer un rango con un tipo de dato {tokens[i][0]}"
+
+    if isinstance(variable_init, type(variable_fin)):
         message = traverse_structure(principal,i,tokens)
         if not message.isdigit():
             return message
