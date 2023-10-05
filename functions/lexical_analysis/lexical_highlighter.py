ca24b1d34bab384c3107e253771ec5fdd5932e53
chizk0 <126604899+chizk0@users.noreply.github.com>
2023-10-05 17:07:56 -0600

    [ADD comments color]

--- functions/lexical_analysis/lexical_highlighter.py
+++ functions/lexical_analysis/lexical_highlighter.py
@@ -6,18 +6,19 @@ class LexicalHighlighter(QSyntaxHighlighter):
     def __init__(self, parent=None):
         super(LexicalHighlighter, self).__init__(parent)
         self.highlighting_rules = []
-        mi_color_personalizado2 = QColor(255, 124, 0)
-        mi_color_personalizado3 = QColor(255, 62, 0)
+        orange_color = QColor(255, 124, 0)
+        red_color = QColor(255, 62, 0)
+        comments_color = QColor(128, 128, 128)
         keyword_formatBlue = QTextCharFormat()
         keyword_formatOrange = QTextCharFormat()
         keyword_formatRed = QTextCharFormat()
         keyword_formatGreen = QTextCharFormat()
         string_format = QTextCharFormat()
-
+        comments_format = QTextCharFormat()
         keyword_formatGreen.setForeground(Qt.green)
-        keyword_formatRed.setForeground(mi_color_personalizado3)
+        keyword_formatRed.setForeground(red_color)
         keyword_formatBlue.setForeground(Qt.blue)
-        keyword_formatOrange.setForeground(mi_color_personalizado2)
+        keyword_formatOrange.setForeground(orange_color)
 
         keywords = ["ENTERO", "FLOTANTE","BOOLEANO", "CADENA", "CARACTER"]  # Agrega aquí más palabras clave si es necesario
         for keyword in keywords:
@@ -43,9 +44,15 @@ class LexicalHighlighter(QSyntaxHighlighter):
 
         
         pattern = r'"[^"]*"' # Reconoce cadenas entre comillas
-        string_format.setForeground(mi_color_personalizado2)  # Color de texto para las cadenas
+        string_format.setForeground(orange_color)  # Color de texto para las cadenas
         rule = (QRegExp(pattern), string_format)
         self.highlighting_rules.append(rule)
+        
+        pattern = r'\$[^$]*\$' # RECONOCE TEXTO ENTRE $ 
+        comments_format.setForeground(comments_color)  # Color de texto para las cadenas
+        rule = (QRegExp(pattern), comments_format)
+        self.highlighting_rules.append(rule)
+
     def highlightBlock(self, text):
         for pattern, format in self.highlighting_rules:
             expression = QRegExp(pattern)
